7709541dcc056147a4b8eb4c8299c7453838b7ed
vitor <vitorcg002@gmail.com>
2019-09-16 10:17:04 -0300

    First Commit

---
+++ src/main/java/com/github/arocketman/entities/Role.java
@@ -0,0 +1,33 @@
+package com.github.arocketman.entities;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+public class Role extends BaseEntity{
+
+    private String name;
+
+    public Role(String user) {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/github/arocketman/entities/User.java
@@ -0,0 +1,65 @@
+package com.github.arocketman.entities;
+
+import javax.persistence.*;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+public class User extends BaseEntity{
+
+    private String username;
+    private String password;
+    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    private List<Role> roles;
+    private boolean active;
+
+    public User(String user, String user1, List<Role> asList, boolean b) {
+        super();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                ", active=" + active +
+                '}';
+    }
+}
